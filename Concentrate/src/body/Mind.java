0ceb3ec6ec5fc96a8368371086c62751b9b10ceb
maximapup (Evan) <ebperkins2@gmail.com>
2026-08-14 18:31:11 -0300

    Inputs and Outputs

--- Concentrate/src/body/Mind.java
+++ Concentrate/src/body/Mind.java
@@ -1,5 +1,28 @@
 package body;
+import java.util.Scanner;
 
 public class Mind {
-//Financial Tracker, start with Amount, Type, Category, Date, Description inputs
+	public static void main (String[] args) {
+//Financial Tracker, start with Amount, Type, Category, Date, Description inputs, and output them to start, then upload to github.
+	Scanner myObj = new Scanner(System.in);
+	float amount = 0; //How much is the amount transacted?
+	String type=""; //Is this income or expense?
+	String category=""; //Groceries, Rent, Salary, etc.
+	String date=""; //MM/DD/YYYY format is preferable
+	String description=""; //A short note detailing what the expense/income is.
+	
+	System.out.println("How much is the amount transacted?");
+	amount = myObj.nextFloat();
+	System.out.println("Is this income or expense?");
+	type = myObj.nextLine();
+	type = myObj.nextLine(); //Redundancy due to nextFloat
+	System.out.println("What type of category can this fit into?  (Groceries, Rent, Salary, etc.)");
+	category = myObj.nextLine();
+	System.out.println("What date did this transaction occur?  (MM/DD/YYYY format preferable)");
+	date = myObj.nextLine();
+	System.out.println("Describe what the transaction is.");
+	description = myObj.nextLine();
+	
+	System.out.println(amount + ", " + type + ", " + category + ", " + date + "; " + description);
+	}
 }
